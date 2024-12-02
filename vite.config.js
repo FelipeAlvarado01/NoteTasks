e64abaa0b95688bba952f419cd4bb3cbb525d59e
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-12-02 01:58:27 -0500

    cambio de ruta

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://felipealvarado01.github.io/NoteTasks/',
+  base:'https://felipealvarado01.github.io/NoteTasks',
 })
