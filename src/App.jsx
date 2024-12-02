c8978b3fea4d5ba02249d3a0bb44d48ee47f47ab
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-12-02 00:06:01 -0500

    input para agregar el nombre de las cartas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import "./App.css";
 import CardTask from "./components/CardTask";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
+let cardId = 0;
 function App() {
+  const [myCards, setMyCard] = useState([]);
+
+  const addNewCardTask = () => {
+    setMyCard([...myCards, { id: cardId++ }]);
+    console.log("agregando nueva tarea");
+  };
+
   return (
     <>
-      <section className="flex w-full h-screen justify-center items-center">
-        <CardTask cardTaskName="My Fisrts Card" />
-      </section>
+      <main className="flex flex-row w-full h-screen justify-evenly items-center">
+        <section className="flex gap-2 overflow-x-auto">
+          {myCards.map((card) => (
+            <CardTask key={card.id} />
+          ))}
+        </section>
+
+        <section>
+          <button
+            onClick={addNewCardTask}
+            className=" bg-slate-200/50 p-4 rounded-md	px-10 py-16  hover:bg-slate-300 shadow-2xl"
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </section>
+      </main>
     </>
   );
 }
--- src/components/CardTask.jsx
+++ src/components/CardTask.jsx
@@ -10,33 +10,76 @@ import { useState } from "react";
 import TaskComponents from "./TaskComponets";
 
 let nextId = 1;
-function CardTask({ cardTaskName }) {
+function CardTask() {
+  const [addTitle, setAddTitle] = useState(false);
+  const [titleCard, setTitleCard] = useState("");
+
   const [noteTask, setNoteTask] = useState("");
   const [tasks, setTasks] = useState([]);
+
   const [showTextarea, setShowTextarea] = useState(false);
 
-  const inputValue = (e) => {
-    setNoteTask(e.target.value);
+  {
+    /*Se agrega el titulo al la carta */
+  }
+  const showInputAddTitle = () => {
+    setAddTitle(!addTitle);
+    console.log("titulo: " + titleCard);
+  };
+  const addTitleCard = (e) => {
+    setTitleCard(e.target.value);
   };
 
-  const addTask = () => {
-    setTasks([...tasks, { id: nextId++, noteTask: noteTask }]);
-  };
-
+  {
+    /* Se muestra el textarea al dar click en el boton + */
+  }
   const showTextareaClick = () => {
     setShowTextarea(!showTextarea);
   };
 
+  {
+    /* Se guarda el texto que se haya introducido en el textarea*/
+  }
+  const inputValue = (e) => {
+    setNoteTask(e.target.value);
+  };
+
+  {
+    /* Se guarda inserta la nueva tarea en el array nombrado como "tasks" para posteriormente ser renderizado*/
+  }
+  const addTask = () => {
+    setTasks([...tasks, { id: nextId++, noteTask: noteTask }]);
+  };
+
   return (
     <>
-      <figure className=" bg-sky-400 w-96 h-96 px-1 shadow-lg rounded-xl ">
+      <figure className=" bg-sky-400 min-w-96 h-96 px-1 shadow-lg rounded-xl ">
         {/* Encabezado de la tarea */}
 
-        <header className="flex flex-wrap items-center justify-stretch border-b-2 py-1">
+        <header className="flex flex-wrap w-full items-center justify-stretch border-b-2 py-1">
           <div className="flex-1">
-            <h1 className="text-2xl text-center text-black font-sans">
-              {cardTaskName}
-            </h1>
+            {addTitle ? (
+              <h1 className="text-2xl text-center text-black font-sans">
+                {titleCard}
+              </h1>
+            ) : (
+              <>
+                <div className="flex gap-0.5 mx-2 p-1 py-3 rounded bg-sky-200 white">
+                  <input
+                    type="text"
+                    onChange={addTitleCard}
+                    placeholder="Ingrese el título del bloc de tareas."
+                    className="w-full bg-sky-200 white	rounded text-base text-zinc-800 placeholder-gray-500	focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  ></input>
+                  <button
+                    onClick={showInputAddTitle}
+                    className="flex justify-center bg-lime-600 rounded-full shadow-lg p-1 "
+                  >
+                    <FontAwesomeIcon icon={faCheck} />
+                  </button>
+                </div>
+              </>
+            )}
           </div>
           <div className="cursor-pointer bg-sky-500 px-1 py-3 rounded-tr-lg shadow-inner">
             <FontAwesomeIcon
@@ -48,7 +91,7 @@ function CardTask({ cardTaskName }) {
         </header>
 
         {/* Detalles de la tarea */}
-        <main className="flex flex-col content-between h-5/6 py-3 px-2 gap-2 	">
+        <div className="flex flex-col content-between h-5/6 py-3 px-2 gap-2 	">
           <ul className="flex flex-col overflow-y-auto h-5/6 gap-y-2">
             {tasks.map((task) => (
               <TaskComponents
@@ -66,7 +109,7 @@ function CardTask({ cardTaskName }) {
                 placeholder="Añade una nueva tarea a tu lista"
                 onChange={inputValue}
                 required
-                className="block w-full bg-sky-200 white	rounded p-1 text-xs text-zinc-800 placeholder-gray-500	focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 "
+                className="w-full h-70 bg-sky-200 white	rounded p-1 text-xs text-zinc-800 placeholder-gray-500	focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 "
               />
             ) : (
               ""
@@ -102,21 +145,9 @@ function CardTask({ cardTaskName }) {
               </button>
             </div>
           </div>
-        </main>
+        </div>
       </figure>
     </>
-  );
-}
-
-function TextareaTasks(inputValue) {
-  return (
-    <textarea
-      type="text"
-      placeholder="Añade una nueva tarea a tu lista"
-      onChange={inputValue}
-      required
-      className="block w-full bg-sky-200 white	rounded p-1 text-xs text-zinc-800 placeholder-gray-500	focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 "
-    />
   );
 }
 export default CardTask;
