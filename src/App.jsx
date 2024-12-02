d11da7aad2c005abc776b570c277e9e9845a32f9
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-12-02 02:35:32 -0500

    nueva distribucion

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-      <main className="flex flex-row w-full h-screen justify-evenly items-center">
+      <main className="flex flex-row w-full h-screen justify-center gap-1 items-center">
         <section className="flex gap-2 relative">
           {myCards.map((card) => (
             <CardTask key={card.id} />
